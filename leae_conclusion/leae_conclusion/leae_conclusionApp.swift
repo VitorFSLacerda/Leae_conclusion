be583a70fa4f47b31165f555cf9bea6dd50ec598
Vitor Lacerda <floco.vitor@gmail.com>
2025-05-05 11:17:39 -0300

    Remover .DS_Store e adicionar ao .gitignore

---
+++ leae_conclusion/leae_conclusion/leae_conclusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  leae_conclusionApp.swift
+//  leae_conclusion
+//
+//  Created by Aluno 50 on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct leae_conclusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
