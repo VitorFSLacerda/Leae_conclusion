9efc4417dbbfcf9f0663346931b3a32f06d983e4
Vitor Lacerda <floco.vitor@gmail.com>
2025-05-06 11:52:12 -0300

    Atualizando atributos da classe

--- leae_conclusion/leae_conclusion/Models/Missao.swift
+++ leae_conclusion/leae_conclusion/Models/Missao.swift
@@ -1,27 +1,62 @@
 import Foundation
 
 class Missao {
+    
+    var dom: Bool
+    var seg: Bool
+    var ter: Bool
+    var qua: Bool
+    var qui: Bool
+    var sex: Bool
+    var sab: Bool
 
+    // Dias consecutivos
     private var _diasConsecutivosAtual: Int
     private var _recordeDiasConsecutivos: Int
-    private var _guardaDataInicioCiclo: Date?
-    private var _guardaDataFinalCiclo: Date?
-    private var _cicloAtivo: Bool
+    private var _guardaDataInicioCicloDia: Date?
+    private var _guardaDataFinalCicloDia: Date?
+
+    // Semanas consecutivas
+    private var _semanasConsecutivasAtual: Int
+    private var _recordeSemConsecutivos: Int
+    private var _guardaDataInicioCicloSem: Date?
+    private var _guardaDataFinalCicloSem: Date?
+    private var _cicloSemanaAnterior: Bool
 
     init(
+        dom: Bool = false, seg: Bool = false, ter: Bool = false, qua: Bool = false,
+        qui: Bool = false, sex: Bool = false, sab: Bool = false,
         diasConsecutivosAtual: Int = 0,
         recordeDiasConsecutivos: Int = 0,
-        dataInicio: Date? = nil,
-        dataFim: Date? = nil,
-        cicloAtivo: Bool = false
+        dataInicioDia: Date? = nil,
+        dataFimDia: Date? = nil,
+        semanasConsecutivasAtual: Int = 0,
+        recordeSemanasConsecutivas: Int = 0,
+        dataInicioSem: Date? = nil,
+        dataFimSem: Date? = nil,
+        cicloSemanaAnterior: Bool = false
     ) {
+        self.dom = dom
+        self.seg = seg
+        self.ter = ter
+        self.qua = qua
+        self.qui = qui
+        self.sex = sex
+        self.sab = sab
+
         self._diasConsecutivosAtual = diasConsecutivosAtual
         self._recordeDiasConsecutivos = recordeDiasConsecutivos
-        self._guardaDataInicioCiclo = dataInicio
-        self._guardaDataFinalCiclo = dataFim
-        self._cicloAtivo = cicloAtivo
+        self._guardaDataInicioCicloDia = dataInicioDia
+        self._guardaDataFinalCicloDia = dataFimDia
+
+        self._semanasConsecutivasAtual = semanasConsecutivasAtual
+        self._recordeSemConsecutivos = recordeSemanasConsecutivas
+        self._guardaDataInicioCicloSem = dataInicioSem
+        self._guardaDataFinalCicloSem = dataFimSem
+        self._cicloSemanaAnterior = cicloSemanaAnterior
     }
-    
+
+    // Getters e Setters para Dias
     var diasConsecutivosAtual: Int {
         get { _diasConsecutivosAtual }
         set { _diasConsecutivosAtual = newValue }
@@ -32,18 +67,39 @@ class Missao {
         set { _recordeDiasConsecutivos = newValue }
     }
 
-    var dataInicioCiclo: Date? {
-        get { _guardaDataInicioCiclo }
-        set { _guardaDataInicioCiclo = newValue }
+    var dataInicioCicloDia: Date? {
+        get { _guardaDataInicioCicloDia }
+        set { _guardaDataInicioCicloDia = newValue }
     }
 
-    var dataFinalCiclo: Date? {
-        get { _guardaDataFinalCiclo }
-        set { _guardaDataFinalCiclo = newValue }
+    var dataFinalCicloDia: Date? {
+        get { _guardaDataFinalCicloDia }
+        set { _guardaDataFinalCicloDia = newValue }
     }
 
-    var cicloAtivo: Bool {
-        get { _cicloAtivo }
-        set { _cicloAtivo = newValue }
+    // Getters e Setters para Semanas
+    var semanasConsecutivasAtual: Int {
+        get { _semanasConsecutivasAtual }
+        set { _semanasConsecutivasAtual = newValue }
+    }
+
+    var recordeSemanasConsecutivas: Int {
+        get { _recordeSemConsecutivos }
+        set { _recordeSemConsecutivos = newValue }
+    }
+
+    var dataInicioCicloSem: Date? {
+        get { _guardaDataInicioCicloSem }
+        set { _guardaDataInicioCicloSem = newValue }
+    }
+
+    var dataFinalCicloSem: Date? {
+        get { _guardaDataFinalCicloSem }
+        set { _guardaDataFinalCicloSem = newValue }
+    }
+
+    var cicloSemanaAnterior: Bool {
+        get { _cicloSemanaAnterior }
+        set { _cicloSemanaAnterior = newValue }
     }
 }
