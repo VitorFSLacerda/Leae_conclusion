14258189f75fb7cefdb381aba0bfdb837fd91dfd
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-05-06 11:13:14 -0300

    updated models' definition

--- leae_conclusion/leae_conclusion/Models/Grupo.swift
+++ leae_conclusion/leae_conclusion/Models/Grupo.swift
@@ -1,32 +1,25 @@
 import Foundation
 
 class Grupo: Codable {
-    // Propriedades principais
+    // Propriedades privadas
     private var _nome: String
     private var _descricao: String
     private var _livro: Livro
     private var _usuarios: [Usuario]
+	private var _comentarios: [Comentario]
     private var _imagem: String
     private var _lider: Usuario?
     private var _missoes: [Missao]
 
     // Construtor
-    init(
-        nome: String,
-        descricao: String,
-        livro: Livro,
-        usuarios: [Usuario] = [],
-        imagem: String,
-        lider: Usuario,
-        missoes: [Missao] = []
-    ) {
+    init(nome: String, descricao: String, livro: Livro, usuarios: [Usuario] = [], imagem: String) {
         self._nome = nome
         self._descricao = descricao
         self._livro = livro
         self._usuarios = usuarios
+		self._comentarios = []
         self._imagem = imagem
-        self._lider = lider
-        self._missoes = missoes
+        self._lider = nil
     }
 
     // MARK: - Propriedades públicas
@@ -63,5 +56,36 @@ class Grupo: Codable {
     var missoes: [Missao] {
         get { return _missoes }
         set { _missoes = newValue }
+    }
+
+    // Mensagem de progresso do líder
+    var mensagemLider: String {
+        if let liderLeu = lider?.progressoEmLivro(livro) {
+            return "\(lider) está liderando: \(liderLeu)% lido!"
+        } else {
+            return "Ninguém começou a leitura ainda."
+        }
+    }
+
+    // Método para adicionar um usuário ao grupo
+    func adicionarUsuario(_ usuario: Usuario) {
+        _usuarios.append(usuario)
+        // Atualiza o líder automaticamente após adicionar um novo usuário
+        definirLider()
+    }
+
+    // Método para remover um usuário do grupo
+    func removerUsuario(_ usuario: Usuario) {
+        _usuarios = _usuarios.filter { $0 !== usuario } // Remove o usuário se ele existir
+        // Atualiza o líder automaticamente após remover um usuário
+        definirLider()
+    }
+
+    // Método para definir o líder com base no progresso de leitura
+    func definirLider() {
+        _lider = _usuarios.max { u1, u2 in
+            // Comparar o progresso de leitura dos usuários no livro do grupo
+            return (u1.progressoEmLivro(self.livro) ?? 0) < (u2.progressoEmLivro(self.livro) ?? 0)
+        }
     }
 }
