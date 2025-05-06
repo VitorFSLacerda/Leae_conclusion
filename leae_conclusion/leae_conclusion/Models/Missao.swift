f67f4a925c0f734710929ec90dacbd39355da89f
Vitor Lacerda <floco.vitor@gmail.com>
2025-05-06 11:31:25 -0300

    Construção da model Missao/Constancia

--- leae_conclusion/leae_conclusion/Models/Missao.swift
+++ leae_conclusion/leae_conclusion/Models/Missao.swift
@@ -1,51 +1,49 @@
-class Missao: Codable {
-    
-    private var _descricao: String
-    private var _pontuacao: String
-    private var _tipo: String
-    private var _progresso: Int
-    private var _meta: Int
-    
-    init(descricao: String, pontuacao: String, tipo: String, meta: Int, progresso: Int = 0) {
-        self._descricao = descricao
-        self._pontuacao = pontuacao
-        self._tipo = tipo
-        self._meta = meta
-        self._progresso = progresso
+import Foundation
+
+class Missao {
+
+    private var _diasConsecutivosAtual: Int
+    private var _recordeDiasConsecutivos: Int
+    private var _guardaDataInicioCiclo: Date?
+    private var _guardaDataFinalCiclo: Date?
+    private var _cicloAtivo: Bool
+
+    init(
+        diasConsecutivosAtual: Int = 0,
+        recordeDiasConsecutivos: Int = 0,
+        dataInicio: Date? = nil,
+        dataFim: Date? = nil,
+        cicloAtivo: Bool = false
+    ) {
+        self._diasConsecutivosAtual = diasConsecutivosAtual
+        self._recordeDiasConsecutivos = recordeDiasConsecutivos
+        self._guardaDataInicioCiclo = dataInicio
+        self._guardaDataFinalCiclo = dataFim
+        self._cicloAtivo = cicloAtivo
     }
     
-    var descricao: String {
-        get { return _descricao }
-        set { _descricao = newValue }
+    var diasConsecutivosAtual: Int {
+        get { _diasConsecutivosAtual }
+        set { _diasConsecutivosAtual = newValue }
     }
-    
-    var pontuacao: String {
-        get { return _pontuacao }
-        set { _pontuacao = newValue }
+
+    var recordeDiasConsecutivos: Int {
+        get { _recordeDiasConsecutivos }
+        set { _recordeDiasConsecutivos = newValue }
     }
-    
-    var tipo: String {
-        get { return _tipo }
-        set { _tipo = newValue }
+
+    var dataInicioCiclo: Date? {
+        get { _guardaDataInicioCiclo }
+        set { _guardaDataInicioCiclo = newValue }
     }
-    
-    var progresso: Int {
-        get { return _progresso }
-        set { _progresso = max(0, newValue) } // impede progresso negativo
+
+    var dataFinalCiclo: Date? {
+        get { _guardaDataFinalCiclo }
+        set { _guardaDataFinalCiclo = newValue }
     }
-    
-    var meta: Int {
-        get { return _meta }
-        set { _meta = newValue }
-    }
-    
-    // Método para adicionar progresso
-    func adicionarProgresso(_ valor: Int) {
-        _progresso += valor
-    }
-    
-    // Método para checar se a missão está concluída
-    func estaConcluida() -> Bool {
-        return _progresso >= _meta
+
+    var cicloAtivo: Bool {
+        get { _cicloAtivo }
+        set { _cicloAtivo = newValue }
     }
 }
