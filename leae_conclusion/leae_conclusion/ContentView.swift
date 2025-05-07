43205e4ffc23df06cf51c299640e8713fe7bdc8c
Felipe <felipealbertosantos@outlook.com>
2025-05-07 08:51:51 -0300

    Add Erika Files

--- leae_conclusion/leae_conclusion/ContentView.swift
+++ leae_conclusion/leae_conclusion/ContentView.swift
@@ -1,21 +1,26 @@
-//
-//  ContentView.swift
-//  leae_conclusion
-//
-//  Created by Aluno 50 on 05/05/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Image("App")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 500, height: 500)
+                .shadow(color: .black.opacity(0.4), radius: 5, x: 0, y: 10)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Hover"))
+        .ignoresSafeArea()
     }
 }
-
+///
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
